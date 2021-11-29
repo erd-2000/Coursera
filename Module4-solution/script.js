3bb0b1b4ca848936e110f7f9fc56bc1d72c8b3e7
Elijah Rudd Dobrin <erudddobrin@gmail.com>
2021-11-28 23:19:08 -0500

    actual final commit

--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "Joann", "Marcy", "Elijah", "Kate", "Jay", "Josh", "Mo", "James", "Olly"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
